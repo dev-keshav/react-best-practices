61c1ac235586b3f3a1c436335fa57e2e8043687d
dev-keshav <keshavkumar9431@gmail.com>
2023-06-19 15:22:09 +0530

    changes in modal popup

--- src/Components/Modal/ModalCompo.js
+++ src/Components/Modal/ModalCompo.js
@@ -1,23 +1,15 @@
 import React, { useState } from "react";
+import Popup from "./Popup";
 
 const ModalCompo = () => {
 
     const [showModal, setShowModal] = useState(false);
     const closePop = () => setShowModal(false);
 
-    const Popup = () => {
-        return (
-            <>
-                <p>Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."</p>
-                <button onClick={closePop}>Close popup</button>
-            </>
-        )
-    }
-
     return (
         <>
             <button onClick={() => setShowModal(true)}>Click popup</button> 
-            {showModal && <Popup /> }
+            { showModal && <Popup closePop={closePop} />  }
         </>
     )
 }
--- src/Components/Modal/Popup.js
+++ src/Components/Modal/Popup.js
@@ -1,4 +1,4 @@
-const Popup = () => {
+const Popup = ({closePop}) => {
     return (
         <>
             <p>Browse through the icons below to find the one you need. The search field supports synonyms—for example, try searching for "hamburger" or "logout."</p>
